a3757a954b70117e55a2effe380c717c2eea348b
Tim Dwyer <tgdwyer@gmail.com>
2024-03-16 13:44:51 +1100

    Refactor forcegraph.ts to use classes and improve code structure

--- src/forcegraph.ts
+++ src/forcegraph.ts
@@ -2,22 +2,49 @@ import * as d3 from 'd3';
 
 d3.csv('data/democracyforsaleFY2022.csv').then((data) => {
     const getParty = (d:any) => d['Party Group'] ? d['Party Group'] : d['Party (specific)'];
-    const nodes = Array.from(new Set(data.flatMap(d => [d['Category'], getParty(d)])), id => ({id}));
-    type Link = {source: string, target: string, value: number};
+    class Link {
+        constructor(public source: string, public target: string, public value: number) {}
+    };
+    class Node {
+        constructor(public id: string, public value: number, public colour: d3.Color) {}
+    }
     const unsortedlinks: Link[] = data.map(d => ({source: d['Category'], target: getParty(d), value: parseInt(d['Value'].replace(/,/g, ''))}));
     const sortedlinks = unsortedlinks.sort((a, b) => a.source === b.source ? a.target.localeCompare(b.target) : a.source.localeCompare(b.source));
     const links = sortedlinks.reduce((a, l) => {
         const prev = a.length - 1;
-        if (prev<0 || a[prev].source !== l.source || a[prev].target !== l.target) a.push(l);
-        else a[prev].value += l.value;
+        if (prev<0 || a[prev].source !== l.source || a[prev].target !== l.target) {
+            a.push(new Link(l.source,l.target,l.value));
+        } else {
+            a[prev].value += l.value;
+        }
         return a;
     },[] as Link[])
+    const nodeMap: Map<string,Node> = new Map();
 
-    console.log(links)
+    function addNode(source: string, value: number, colour: string) {
+        const node = nodeMap.get(source);
+        if(node) {
+            node.value += value;
+        } else {
+            const node = new Node(source, value, d3.color(colour)!);
+            nodeMap.set(source,node);
+        }
+    }
+    links.forEach(l => {
+        addNode(l.source,l.value, 'grey');
+        const colour = l.target == 'Liberal/Nationals' ? 'blue' 
+                      :l.target == 'Labor' ? 'red'
+                      :l.target == 'Greens' ? 'green'
+                      :'#fedb89';
+        addNode(l.target,l.value, colour);
+    })
+    const nodes = Array.from(nodeMap.values());
+
     // Create a SVG container for the graph
     const svg = d3.select('body').append('svg')
         .attr('width', window.innerWidth)
         .attr('height', window.innerHeight);
+    const container = svg.append('g');
 
     // Create a D3 force simulation
     const simulation = d3.forceSimulation(nodes)
@@ -29,7 +56,7 @@ d3.csv('data/democracyforsaleFY2022.csv').then((data) => {
         .domain([0, d3.max(links, l => l.value)])
         .range([1, 10])
     // Create SVG elements for nodes and links
-    const link = svg.append('g')
+    const link = container.append('g')
         .selectAll('line')
         .data(links)
         .enter().append('line')
@@ -37,15 +64,20 @@ d3.csv('data/democracyforsaleFY2022.csv').then((data) => {
             return scale(d.value);
         });
 
-    const node = svg.append('g')
+    const node = container.append('g')
         .selectAll('circle')
         .data(nodes)
         .enter().append('circle')
-        .attr('r', 5)
+        .attr('r', d => { 
+            const r = 5 + scale(d.value);
+            d.radius = r;
+            return r;
+        })
         .call(d3.drag()
             .on('start', dragStarted)
             .on('drag', dragged)
-            .on('end', dragEnded));
+            .on('end', dragEnded))
+        .style('fill', d => d.colour.toString());
 
     function dragStarted(event, d) {
         if (!event.active) simulation.alphaTarget(0.3).restart();
@@ -65,7 +97,7 @@ d3.csv('data/democracyforsaleFY2022.csv').then((data) => {
     }
 
     // Add labels to the nodes
-    const labels = svg.append('g')
+    const labels = container.append('g')
         .selectAll('text')
         .data(nodes)
         .enter().append('text')
@@ -86,6 +118,28 @@ d3.csv('data/democracyforsaleFY2022.csv').then((data) => {
         labels.attr('x', d => d.x + 10)
             .attr('y', d => d.y + 5);
         
+    });
+    simulation.on('end', () => {
+        const border = 10,
+            [windowWidth, windowHeight] = [window.innerWidth, window.innerHeight],
+            maxX = Math.max(...Array.from(labels.nodes()).map(l => l.getBBox().x + l.getBBox().width + border)),
+            maxY = Math.max(...nodes.map(v => v.y + v.radius))+border,
+            minX = Math.min(...nodes.map(v => v.x - v.radius))-border,
+            minY = Math.min(...nodes.map(v => v.y - v.radius))-border,
+            [graphWidth, graphHeight] = [maxX - minX, maxY - minY],
+            [scaleX, scaleY] = [windowWidth / graphWidth, windowHeight / graphHeight],
+            scale = Math.min(scaleX, scaleY),
+            [scaledGraphWidth, scaledGraphHeight] = [graphWidth, graphHeight].map(v => v*scale),
+            [centerOffsetX, centerOffsetY] = [windowWidth - scaledGraphWidth, windowHeight - scaledGraphHeight].map(v=>v/2),
+            [windowMinX, windowMinY] = [minX, minY].map(v => v*scale),
+            [tx, ty] = [centerOffsetX - windowMinX, centerOffsetY - windowMinY];
+    
+        // Define the zoom behavior
+        const zoom = d3.zoom()
+            //.scaleExtent([1, 10]) // Adjust scale extent as needed
+            .on('zoom', event => container.attr('transform', event.transform)); 
+        // Apply the zoom behavior to the SVG
+        svg.call(<any>zoom).call(<any>zoom.transform, d3.zoomIdentity.translate(tx,ty).scale(scale))
     });
 
     // Start the simulation
